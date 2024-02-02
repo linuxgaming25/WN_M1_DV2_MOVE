6069d399d4ae1e7459d8fa21e35dcf2887a16cfd
linuxgaming25 <127547557+linuxgaming25@users.noreply.github.com>
2024-02-02 03:24:43 -0700

    Build change

--- WN_M1_DV2_MOVE/Assets/Script/PlayerBehavior.cs
+++ WN_M1_DV2_MOVE/Assets/Script/PlayerBehavior.cs
@@ -2,28 +2,43 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerBehaviour : MonoBehaviour
 {
-    // 1
     public float moveSpeed = 10f;
     public float rotateSpeed = 75f;
 
-    // 2
     private float vInput;
     private float hInput;
-    
-    void Update()
+
+    // 1
+    private Rigidbody _rb;
+
+    // 2
+    private void Start()
     {
         // 3
-        vInput = Input.GetAxis("Vertical") * moveSpeed;
+        _rb = GetComponent<Rigidbody>();
+    }
 
-        // 4
+    void Update()
+    {
+        vInput = Input.GetAxis("Vertical") * moveSpeed;
         hInput = Input.GetAxis("Horizontal") * rotateSpeed;
 
-        // 5
+        /* 4
         this.transform.Translate(Vector3.forward * vInput * Time.deltaTime);
-
-        // 6
         this.transform.Rotate(Vector3.up * hInput * Time.deltaTime);
+        */
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 rotation = Vector3.up * hInput;
+
+        Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+
+        _rb.MovePosition(this.transform.position + this.transform.forward * vInput * Time.fixedDeltaTime);
+
+        _rb.MoveRotation(_rb.rotation * angleRot);
     }
 }
